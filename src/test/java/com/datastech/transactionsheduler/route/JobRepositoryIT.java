7739555953b6633dc20620866c943d279a556108
ivan_salivon <isalivon@datas_tech.com>
2017-11-24 18:05:24 +0200

    add client mongoDB

--- src/test/java/com/datastech/transactionsheduler/route/JobRepositoryIT.java
+++ src/test/java/com/datastech/transactionsheduler/route/JobRepositoryIT.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 /**
  *
@@ -17,6 +18,7 @@ import org.springframework.boot.test.context.SpringBootTest;
  */
 @RunWith(CamelSpringBootRunner.class)
 @SpringBootTest(classes = TransactionShedulerApplication.class)
+@ActiveProfiles(profiles = "test")
 public class JobRepositoryIT {
 
     @Autowired
