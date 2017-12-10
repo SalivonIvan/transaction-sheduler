cbf001f6d752e4c94e3fee40e73570050c5e2fe7
--replace-all <salivon_ivan@ukr.net>
2017-12-10 22:42:20 +0200

    extend dto

--- src/main/java/com/datastech/transactionsheduler/dto/ShedulerDTO.java
+++ src/main/java/com/datastech/transactionsheduler/dto/ShedulerDTO.java
@@ -3,6 +3,8 @@ package com.datastech.transactionsheduler.dto;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Map;
 
 /**
  * Created by ivan salivon on 07.12.17.
@@ -12,8 +14,25 @@ public class ShedulerDTO implements Serializable {
 
     private static final long serialVersionUID = -1193629980047872623L;
 
-    private String routeId;
+    private String shedulerId;
     private String status;
     private String cron;
     private String action;
+    private String groupName;
+    private String triggerName;
+    private boolean stateful;
+    private boolean fireNow;
+    private boolean deleteJob = true;
+    private boolean pauseJob;
+    private boolean durableJob;
+    private boolean recoverableJob;
+    private long triggerStartDelay = 500;
+    private int startDelayedSeconds;
+    private boolean autoStartScheduler = true;
+    private boolean usingFixedCamelContextName;
+    private boolean prefixJobNameWithEndpointId;
+    private Map<String, Object> triggerParameters;
+    private Map<String, Object> jobParameters;
+    private Calendar customCalendar;
+
 }
