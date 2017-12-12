b5d3d3ddd763e8c858f046ccdc998762d45fea3a
ivan_salivon <isalivon@datas_tech.com>
2017-12-12 17:11:40 +0200

    refactor

--- src/main/java/com/datastech/transactionsheduler/route/ShedulerRoute.java
+++ src/main/java/com/datastech/transactionsheduler/route/ShedulerRoute.java
@@ -4,8 +4,6 @@ import org.apache.camel.builder.RouteBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
-
 @Component
 public class ShedulerRoute extends RouteBuilder {
 
@@ -18,8 +16,8 @@ public class ShedulerRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("quartz2://"+timerName +"?cron="+ trigger+"&fireNow=true").routeId(timerName)
+        from("quartz2://" + timerName + "?cron=" + trigger + "&fireNow=true").routeId(timerName)
                 .to(jobUri)
-                .log("JOB["+jobUri+"] for iba sheduler transfer was invoke");
+                .log("JOB[" + jobUri + "] for iba sheduler transfer was invoke");
     }
 }
